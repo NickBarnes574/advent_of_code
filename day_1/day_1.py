edeee13da3b1500d74ff778bdf2f6d5e25fb3cb8
Nick Barnes <nick.a.barnes44@gmail.com>
2023-12-02 17:49:20 -0500

    solve part 1

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
+import os
+import sys
+
+URL = "https://adventofcode.com/2023/day/1/input"
+FILEPATH = "day_1/data.txt"
 
 
 def main():
-    pass
+    total = 0
+
+    with open(FILEPATH, "r") as file:
+        for line in file:
+            first = ""
+            last = ""
+            for char in line:
+                if char.isdigit():
+                    if first == "":
+                        first = char
+                    last = char
+            total += int(first + last)
+
+            print(f"total: {total}")
 
 
 if __name__ == "__main__":
     try:
-        main
+        main()
     except Exception as e:
         print(f"An error occurred: {e}")
